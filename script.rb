1ed320b529c14c6933df4bc03a565fbf3de024db
PettLe <90179827+PettLe@users.noreply.github.com>
2025-10-27 12:20:33 +0200

    finished

--- script.rb
+++ script.rb
@@ -1,31 +1,27 @@
 def bubble_sort(array)
   index = 0
   not_ready = true
-  rounds = 0
+  rounds = 1
 
   while not_ready
     if array[index] <= array[index +1]
-      p [array[index], array[index +1]]
+      rounds += 1
     else
-      p [array[index], array[index +1]].rotate(1)
       array.insert(index +1, array.delete_at(index))
     end
-    
+
+    if rounds == array.length
+      not_ready = false
+    end
+
     index += 1
     if index == array.length - 1
       index = 0
-      rounds += 1
-    end
-
-    if rounds == 10
-        p array
-        not_ready = false
+      rounds = 1
     end
   end
+  p array
 end
 
 bubble_sort([4,3,78,2,0,2])
-# Ekan iteraation jälkeen:
-# [3,4,2,0,2,78]
-# Lopulta:
-# [0,2,2,3,4,78]
+bubble_sort([6,8,4,987,2,4,2,66,7,0,2,5])
